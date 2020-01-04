f3cc437447e06a82bd4f2d29ea0488e1d52c2848
Vishrut G <vishrut.guggilla@gmail.com>
2020-01-03 22:10:33 -0800

    New 2 Block Auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedFarSidex2.java
@@ -0,0 +1,137 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.AutoBackend.CustomSkystoneDetector;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvInternalCamera;
+
+@Autonomous(name="Red Far Side")
+public class RedFarSidex2 extends LinearOpMode {
+    private ElapsedTime runtime = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
+    public double robotSpeed = 0.45;
+    OmegaBot robot;
+    OmegaPID drivePID;
+    MotionMethods motionMethods;
+    private OpenCvCamera phoneCam;
+    private CustomSkystoneDetector skyStoneDetector;
+
+    String skystonePosition = "none";
+    double xPosition;
+    double yPosition;
+
+    public void runOpMode() {
+        robot = new OmegaBot(telemetry, hardwareMap);
+        drivePID = robot.drivePID;
+        motionMethods = new MotionMethods(robot, telemetry, this);
+        /*
+         */
+        robot.drivetrain.setRunMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.drivetrain.setRunMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        /*
+         * Instantiate an OpenCvCamera object for the camera we'll be using.
+         * In this sample, we're using the phone's internal camera. We pass it a
+         * CameraDirection enum indicating whether to use the front or back facing
+         * camera, as well as the view that we wish to use for camera monitor (on
+         * the RC phone). If no camera monitor is desired, use the alternate
+         * single-parameter constructor instead (commented out below)
+         */
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        phoneCam = new OpenCvInternalCamera(OpenCvInternalCamera.CameraDirection.FRONT, cameraMonitorViewId);
+
+        // OR...  Do Not Activate the Camera Monitor View
+        //phoneCam = new OpenCvInternalCamera(OpenCvInternalCamera.CameraDirection.BACK);
+
+        /*
+         * Open the connection to the camera device
+         */
+        phoneCam.openCameraDevice();
+
+        /*
+         * Specify the image processing pipeline we wish to invoke upon receipt
+         * of a frame from the camera. Note that switching pipelines on-the-fly
+         * (while a streaming session is in flight) *IS* supported.
+         */
+        skyStoneDetector = new CustomSkystoneDetector();
+        skyStoneDetector.useDefaults();
+        phoneCam.setPipeline(skyStoneDetector);
+
+        /*
+         * Tell the camera to start streaming images to us! Note that you must make sure
+         * the resolution you specify is supported by the camera. If it is not, an exception
+         * will be thrown.
+         *
+         * Also, we specify the rotation that the camera is used in. This is so that the image
+         * from the camera sensor can be rotated such that it is always displayed with the image upright.
+         * For a front facing camera, rotation is defined assuming the user is looking at the screen.
+         * For a rear facing camera or a webcam, rotation is defined assuming the camera is facing
+         * away from the user.
+         */
+        phoneCam.startStreaming(320, 240, OpenCvCameraRotation.SIDEWAYS_LEFT);
+
+        // number of inches the robot strafes back depending
+        // on the position of the skystone closest to the Skybridge
+        double back = 0;
+        //All comments comment above what is being commented
+
+        while (!isStopRequested() && !opModeIsActive()) {
+            xPosition = skyStoneDetector.foundRectangle().x;
+            yPosition = skyStoneDetector.foundRectangle().y;
+
+            if (xPosition >= 80) { //TODO Tune these numbers
+                skystonePosition = "right";
+                back = 4;
+            } else if (xPosition > 2) {//x = 12
+                skystonePosition = "center";
+                back = 0;
+            } else {
+                skystonePosition = "left";
+                back = 10;
+            }
+
+            telemetry.addData("xPos", xPosition);
+            telemetry.addData("yPos", yPosition);
+            telemetry.addData("SkyStone Pos", skystonePosition);
+            telemetry.update();
+        }
+        waitForStart();
+
+        // set initial values for arm, block gripper, pivot, and intakes
+        robot.arm.setTargetPosition(-400);
+        robot.arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.arm.setPower(.5);
+        robot.blockGripper.setPosition(.75);
+        robot.pivot.setPosition(.62);
+        sleep(500);
+        robot.leftIntake.setPower(-1);
+        robot.rightIntake.setPower(1);
+
+        motionMethods.strafe(180, .4, 1);
+
+        //turns to angle and moves
+        motionMethods.moveMotionProfile(back, 1);
+        motionMethods.turnUsingPIDVoltageFieldCentric(110, .5);
+        motionMethods.moveMotionProfile(28, 1);
+
+        robot.arm.setTargetPosition(0);
+        robot.arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.arm.setPower(.5);
+        robot.blockGripper.setPosition(0.2);
+        robot.leftIntake.setPower(0);
+        robot.rightIntake.setPower(0);
+        sleep(500);
+
+        //faces red wall and strafes to building
+        motionMethods.turnUsingPIDVoltageFieldCentric(0,.5);
+        robot.arm.setTargetPosition(-110);
+        robot.arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.arm.setPower(.5);
+        motionMethods.strafe(0, 1, 1);
+        motionMethods.moveMotionProfile(60,1);
+    }
+}
