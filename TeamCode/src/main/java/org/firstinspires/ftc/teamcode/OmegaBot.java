46a9948925d52b50e934da485863730c288d92fd
nidhirg <nidhirg2014@gmail.com>
2020-02-18 11:34:54 -0800

    puahing vivas updatea

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OmegaBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OmegaBot.java
@@ -1,9 +1,14 @@
 package org.firstinspires.ftc.teamcode;
 
+import android.app.Activity;
+import android.view.View;
+
 import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -39,9 +44,16 @@ public class OmegaBot {
     public Servo pivot;
     public Servo blockGripper;
     public Servo centerGripper;
-    public Servo cap;//0 open, 0.5 closed, this is actually elbow gripper
+    public Servo cap;//0.41 open, 0.05 closed, this is actually elbow gripper
     public Servo rightGripper;//0.33 up, 0 down
     public Servo elbowGripper;//this is actually capstone, its just that i configged wrong
+    public Servo frontElbow;
+    public Servo frontWrist;//.91 open, .56 closed
+    public Servo capstone;//.9 holding the capstone, .28 dropping the capstone
+    public ColorSensor sensorColor;
+    public DistanceSensor sensorDistance;
+    public int relativeLayoutId;
+    public View relativeLayout;
 
 
     DcMotor.RunMode myRunMode = DcMotor.RunMode.RUN_USING_ENCODER;
@@ -75,11 +87,19 @@ public class OmegaBot {
         leftIntake = hardwareMap.get(DcMotor.class, "left_intake");
         rightIntake = hardwareMap.get(DcMotor.class, "right_intake");
 
+
         pivot = hardwareMap.get(Servo.class, "pivot");
         blockGripper = hardwareMap.get(Servo.class, "block_gripper");
         centerGripper = hardwareMap.get(Servo.class, "center_gripper");
         cap = hardwareMap.get(Servo.class, "cap");
         rightGripper = hardwareMap.get(Servo.class, "left_gripper");
+        frontElbow = hardwareMap.get(Servo.class, "front_elbow");
+        frontWrist = hardwareMap.get(Servo.class, "front_wrist");
+        capstone = hardwareMap.get(Servo.class, "capstone");
+        sensorDistance = hardwareMap.get(DistanceSensor.class, "color_distance_sensor");
+        sensorColor = hardwareMap.get(ColorSensor.class, "color_distance_sensor");
+        relativeLayoutId = hardwareMap.appContext.getResources().getIdentifier("RelativeLayout", "id", hardwareMap.appContext.getPackageName());
+        relativeLayout = ((Activity) hardwareMap.appContext).findViewById(relativeLayoutId);
         elbowGripper = hardwareMap.get(Servo.class, "capstone");//port number 5
         // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
         // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
